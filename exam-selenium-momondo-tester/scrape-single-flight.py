efba0b9cdc0d3756e14ed4a232bba8393eb47098
Mathias2860DK <mathias.enemark.poulsen@gmail.com>
2022-05-09 10:45:06 +0200

    data exploring

--- exam-selenium-momondo-tester/scrape-single-flight.py
+++ exam-selenium-momondo-tester/scrape-single-flight.py
@@ -7,6 +7,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from bs4 import BeautifulSoup
 import os
+import datetime
 
 # Change this to your own chromedriver path!
 driver = webdriver.Chrome()  # looks in /usr/local/bin
@@ -23,9 +24,11 @@ def srape_flights(from_location, to_location, from_date, flexible=False):
 
     # close pop up
     # Body id changes every time site loads - So we are using full xpath
-
-    class_popup_close = '//div[@class = "dDYU-close dDYU-mod-variant-default dDYU-mod-size-default"]'
-    driver.find_element_by_xpath(class_popup_close).click()
+    try:
+        class_popup_close = '//div[@class = "dDYU-close dDYU-mod-variant-default dDYU-mod-size-default"]'
+        driver.find_element_by_xpath(class_popup_close).click()
+    except:
+        pass
 
     out_time_list = []
 
@@ -86,7 +89,7 @@ def srape_flights(from_location, to_location, from_date, flexible=False):
 
     flight_rows = driver.find_elements_by_xpath(
         '//div[@class="resultWrapper"]')
-    print(flight_rows)
+    #print(flight_rows)
     # convert flights_rows to html so beatufiul soup can understand it
     for WebElement in flight_rows:
 
@@ -129,19 +132,18 @@ def srape_flights(from_location, to_location, from_date, flexible=False):
         first_flight_times = both_schedules[0].find(
         "div", {"class": "top"})
         out_time = first_flight_times.text.replace('\n', '')
-        print('out time: ' + out_time)
+        #print('out time: ' + out_time)
         out_time_list.append(out_time)
 
         out_stop = both_section_stops[0].text.replace('\n', '')
-        print(out_stop)
+        #print(out_stop)
         out_stops.append(out_stop)
 
         out_duration = both_durations[0].text.replace('\n', '')
-        print(out_duration)
+        #print(out_duration)
         out_durations.append(out_duration)
 
-                # time hardcoded for now
-        out_dates.append('14 juli')
+        out_dates.append(from_date)
 
            
     cols = (['Destination','Out Date', 'Out Duration',
@@ -162,13 +164,36 @@ def srape_flights(from_location, to_location, from_date, flexible=False):
     return flights_df
 
 
+# consider the start date as 2021-february 1 st
+start_date = datetime.date(2022, 7, 19)
+
+# consider the end date as 2021-march 1 st
+end_date = datetime.date(2022, 8, 15)
+
+# delta time
+delta = datetime.timedelta(days=1)
+
 # 63cy = hele cypern
-to_location = '94cy'
-final_df = srape_flights('CPH', to_location, '2022-07-14')
-csv_path = os.getcwd() + \
-    "/exam-selenium-momondo-tester/exam-data/flight-scrape-single-5-maj-{to_location}.csv".format(to_location=to_location
-)
-final_df.to_csv(csv_path)
+to_location = '63cy'
+# iterate over range of dates
+while (start_date <= end_date):
+    from_date = start_date.strftime("%Y-%m-%d")
+    print('srcaping data from', from_date, "to Cypern", end="\n")
+    final_df = srape_flights('CPH', to_location, from_date)
+    csv_path = os.getcwd() + \
+    "/exam-selenium-momondo-tester/exam-data/flight-scrape-cypern/flight-scrape-{to_location}-{from_date}.csv".format(
+        to_location=to_location, from_date=from_date)
+    final_df.to_csv(csv_path)
+
+    start_date += delta
+    
+# 63cy = hele cypern
+to_location = '63cy'
+from_date = '2022-07-17'
+# final_df = srape_flights('CPH', to_location, from_date)
+# csv_path = os.getcwd() + \
+#     "/exam-selenium-momondo-tester/exam-data/flight-scrape-cypern/flight-scrape-{to_location}-{from_date}.csv".format(to_location=to_location, from_date=from_date)
+# final_df.to_csv(csv_path)
 # class = resultwrapper
 # class = inner-grid keel-grid
 # for price: class = col-price result-column js-no-dtog
